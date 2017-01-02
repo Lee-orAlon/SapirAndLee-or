b19cf9e8fcfe53dfad573432852de659d5635935
Lee-or <lee-or@mail.com>
2017-01-03 01:04:25 +0200

    Client and Server are updated- veraion 2- now client and serve can communicate with each other.

---
+++ Server.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include "MainFlow.h"
+
+using namespace std;
+
+int main(int argc, char**argv) {
+    MainFlow *mainFlow = new MainFlow();
+    const int server_port = atoi(argv[1]);
+
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) {
+        perror("error creating socket");
+    }
+
+    struct sockaddr_in sin;
+
+    memset(&sin, 0, sizeof(sin));
+
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons(server_port);
+
+    if (bind(sock, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
+        perror("error binding to socket");
+    }
+
+    struct sockaddr_in from;
+    unsigned int from_len = sizeof(struct sockaddr_in);
+
+    char buffer[4096];
+
+    while (mainFlow->doUserRequest() != 7) {
+        int bytes = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr *) &from, &from_len);
+        if (bytes < 0) {
+            perror("error reading from socket");
+        }
+
+        cout << "The client sent: " << buffer << endl;
+
+        int sent_bytes = sendto(sock, buffer, bytes, 0, (struct sockaddr *) &from, sizeof(from));
+        if (sent_bytes < 0) {
+            perror("error writing to socket");
+        }
+        buffer[0]='a';
+    }
+    close(sock);
+
+    return 0;
+}
