575f7722bacef6a6cee4b3ba0e7bb93cd051a523
Lee-or <lee-or@mail.com>
2017-01-03 22:30:17 +0200

    Client and server are updated- version 3- now the server can close the socket.

--- Client.cpp
+++ Client.cpp
@@ -8,6 +8,9 @@
 using namespace std;
 int main(int argc, char**argv) {
     if (argc == 3) {
+        int sent_bytes;
+        int bytes;
+        string closeSocket = "close";
         const char *ip_address = argv[1];
         const int port_no = atoi(argv[2]);
         int sock = socket(AF_INET, SOCK_DGRAM, 0);
@@ -19,23 +22,30 @@ int main(int argc, char**argv) {
         sin.sin_family = AF_INET;
         sin.sin_addr.s_addr = inet_addr(ip_address);
         sin.sin_port = htons(port_no);
-        char data[] = "hello";
+        char data[] = "hello!";
         int data_len = sizeof(data);
-        while (1) {
-            int sent_bytes = sendto(sock, data, data_len, 0, (struct sockaddr *) &sin, sizeof(sin));
+        bool socketOpen = true;
+        while (socketOpen) {
+            sent_bytes = sendto(sock, data, data_len, 0, (struct sockaddr *) &sin, sizeof(sin));
             if (sent_bytes < 0) {
                 perror("error writing to socket");
+            }
+            if (sent_bytes==EPIPE){
+                socketOpen = false;
             }
             struct sockaddr_in from;
             unsigned int from_len = sizeof(struct sockaddr_in);
             char buffer[4096];
-            int bytes = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr *) &from,
-                                 &from_len);
+            bytes = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr *) &from,
+                             &from_len);
             if (bytes < 0) {
                 perror("error reading from socket");
             }
             if (bytes == 0){
-                close(sock);
+                socketOpen = false;
+            }
+            if(closeSocket.compare(buffer)==0){
+                socketOpen = false;
             }
             cout << "The server sent: " << buffer << endl;
         }
